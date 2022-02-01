26942720113cfc828dde09d17e7238f49585a026
Radwa1s <radwa0168@gmail.com>
2022-02-01 13:21:29 +0200

    first commit

---
+++ src/Components/moviesData.js
@@ -0,0 +1,49 @@
+export const moviesData = [
+    {
+        id: Math.random(),
+        image: 'https://robbinsrealm.files.wordpress.com/2013/05/12-angry-men-pic-1.jpg',
+        rating: 5,
+        name: '12 Angry Men',
+        date: '1957',
+        description:
+            'The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before jumping to a hasty verdict.',
+    },
+    {
+        id: Math.random(),
+        image: 'https://el7l.online:2096/uploads/articles/342abefc.jpg',
+        rating: 4,
+        name: 'One Flew Over the Cuckoo`s Nest',
+        date: '1957',
+        description:
+            'A criminal pleads insanity and is admitted to a mental institution, where he rebels against the oppressive nurse and rallies up the scared patients.',
+    },
+    {
+        id: Math.random(),
+        image: 'https://i.pinimg.com/originals/eb/c7/0a/ebc70a257677e9019aac7cf5848956d8.jpg',
+        rating: 4,
+        name: 'The Seventh Seal',
+        date: '1957',
+        description:
+            "A knight returning to Sweden after the Crusades seeks answers about life, death, and the existence of God as he plays chess against the Grim Reaper during the Black Plague.",
+    },
+    {
+        id: Math.random(),
+        image: 'https://ih1.redbubble.net/image.256850342.4211/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg',
+        rating: 3,
+        name: 'Girl, Interrupted',
+        date: '1999',
+        description:
+            "Based on writer Susanna Kaysen's account of her 18-month stay at a mental hospital in the late 1960s.",
+    },
+    {
+        id: Math.random(),
+        image:
+            'https://m.media-amazon.com/images/I/51dPIsyVaxL._AC_.jpg',
+        rating: 5,
+        name: 'eternal sunshine of the spotless mind poster',
+        date: '2004',
+        description:
+            "When their relationship turns sour, a couple undergoes a medical procedure to have each other erased from their memories.",
+    },
+
+];
